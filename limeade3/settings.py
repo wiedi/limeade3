0c08d0127ac6c19dd24e915d2efed592feae412e
Sebastian Wiedenroth <wiedi@frubar.net>
2014-07-25 17:53:46 +0200

    auth is done by nginx

--- limeade3/settings.py
+++ limeade3/settings.py
@@ -85,7 +85,8 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
 	#'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
-	'PAGINATE_BY': 10
+	'PAGINATE_BY': 10,
+	'DEFAULT_AUTHENTICATION_CLASSES': [],
 }
 
 STATIC_ROOT = BASE_DIR + '/static/'
