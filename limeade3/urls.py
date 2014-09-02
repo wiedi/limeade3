6f69487ce0530088fdd9c9c96c4b56dca1945fe1
Sebastian Wiedenroth <wiedi@frubar.net>
2014-09-02 17:02:10 +0200

    redirect to mailapi from startpage

--- limeade3/urls.py
+++ limeade3/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 admin.autodiscover()
 
 urlpatterns = patterns('',
+	url(r'^$', RedirectView.as_view(url='/mail/')),
 	url(r'^mail/', include('mail.urls')),
-
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 	
