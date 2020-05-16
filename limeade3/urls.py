5f7cfb3215b01b970c3abbf11929c12619f590eb
Thomas Merkel <tm@core.io>
2020-05-16 20:33:09 +0200

    Correct the include for admin url

--- limeade3/urls.py
+++ limeade3/urls.py
@@ -6,6 +6,6 @@ admin.autodiscover()
 urlpatterns = [
 	url(r'^$', RedirectView.as_view(url='/mail/')),
 	url(r'^mail/', include('mail.urls')),
-#	url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/', admin.site.urls),
 	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]	
